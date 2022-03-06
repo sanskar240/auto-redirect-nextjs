89d816f11e8eb1c979eb864bf7a530daafaaa9e0
sanskar240 <71585678+sanskar240@users.noreply.github.com>
2022-03-06 21:43:54 +0530

    Update about.js

--- pages/about.js
+++ pages/about.js
@@ -1,10 +1,15 @@
 import next from "next";
 import Link from "next/link";
-
+import Head from "next/head";
 
 
 const About  = () =>{
     return (
+        <>
+        <Head>
+    <title>Ninja Listing | About</title>
+    <meta name="jobs"  content="hi"/>
+    </Head>
         <div>
             <h1>About Me Page</h1>
             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam leo neque, accumsan facilisis est non, gravida euismod mi. Vivamus tincidunt dictum ipsum, sit amet rutrum massa feugiat laoreet. Nullam suscipit mi a tellus suscipit, ultrices vehicula elit feugiat. Vivamus tincidunt libero non augue scelerisque ultrices. Aenean facilisis, felis a tincidunt aliquet, libero magna malesuada orci, commodo egestas turpis est non massa. Duis in risus eu leo semper ultrices. Mauris nec felis diam. Sed in tellus sodales urna interdum feugiat. Nullam pellentesque pulvinar posuere. Ut suscipit, eros quis commodo dapibus, enim ex vestibulum est, id lobortis tortor ligula maximus ipsum.</p>
@@ -13,6 +18,7 @@ const About  = () =>{
             </div>
             
         </div>
+        </>
     );
 }
 export default About;
